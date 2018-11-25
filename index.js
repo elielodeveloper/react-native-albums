b1eca763565ff8ebbe66ca313a00737aa0e32a62
Eliel Roger <elielrogernic@gmail.com>
2018-11-24 23:25:54 -0200

    Add card and header components

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 //Import a library to help create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { View, AppRegistry } from 'react-native';
 
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 //Create a component
-const App = () => <Text>Some Text</Text>;
+const App = () => (
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
+);
 
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);
