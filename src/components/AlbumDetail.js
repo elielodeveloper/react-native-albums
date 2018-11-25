5d7b6e20bf71fb3fdfc6bfaa2e59946a55ca23ba
Eliel Roger <elielrogernic@gmail.com>
2018-11-25 00:11:23 -0200

    refactor(album-list): Add a ScrollView for album list component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,31 +5,52 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
+  const { title, artist, thumbnail_image, image } = album;
 
   return (
     <Card>
       <CardSection style={style.cardSection}>
-        <View>
-          <Image source={{ uri: thumbnail_image }} />
+        <View style={style.thmbnailContainerStyle}>
+          <Image style={style.thumbnailStyle} source={{ uri: thumbnail_image }} />
         </View>
         <View style={style.headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={style.headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image style={style.imageStyle} source={{ uri: image }}/>
       </CardSection>
     </Card>
   );  
 };
 
 const style = {
-  cardSection = {
+  cardSection: {
     flexDirection: 'row',
     justifyContent: 'flex-start'
-  }
-  headerContentStyle = {
+  },
+  headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thmbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 }
 
